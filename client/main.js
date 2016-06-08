216230485f39adcc460f62775e2e96b71dace467
RobStek <rob.stekelenburg@idgis.nl>
2016-06-08 17:13:03 +0200

    adding books and autoform

--- client/main.js
+++ client/main.js
@@ -1,11 +1,15 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+//import { ReactiveVar } from 'meteor/reactive-var';
+
+import { Books } from '../imports/api/books.js';
 
 import './main.html';
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  
 });
 
 Template.hello.helpers({
@@ -18,5 +22,19 @@ Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+    
+    const book = ({
+      author: "AUT",
+      title: "TIT",
+      copies: instance.counter.get(),
+      lastCheckedOut: new Date(),
+      summary: "SUM"
+    });
+    
+    Books.insert(book);
+    
   },
 });
+
+
+
